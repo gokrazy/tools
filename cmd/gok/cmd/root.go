d3bc434581576b8fcfca961a36eae20661ac4d63
Michael Stapelberg <michael@stapelberg.de>
2022-10-02 21:00:20 +0200

    implement gok --version

--- cmd/gok/cmd/root.go
+++ cmd/gok/cmd/root.go
@@ -1,9 +1,12 @@
 package cmd
 
 import (
+	"fmt"
 	"log"
+	"runtime/debug"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
 )
 
 var RootCmd = &cobra.Command{
@@ -14,6 +17,33 @@ directory, building and running Go programs from your local Go workspace,
 etc.`,
 	SilenceErrors: true,
 	SilenceUsage:  true,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		versionVal, err := cmd.Flags().GetBool("version")
+		if err != nil {
+			return fmt.Errorf("BUG: version flag declared as non-bool")
+		}
+		if versionVal {
+			fmt.Println(version())
+			return nil
+		}
+		return pflag.ErrHelp
+	},
+}
+
+func version() string {
+	info, ok := debug.ReadBuildInfo()
+	if !ok {
+		return "<not okay>"
+	}
+	settings := make(map[string]string)
+	for _, s := range info.Settings {
+		settings[s.Key] = s.Value
+	}
+	modified := ""
+	if settings["vcs.modified"] == "true" {
+		modified = " (modified)"
+	}
+	return "https://github.com/gokrazy/tools/commit/" + settings["vcs.revision"] + modified
 }
 
 func Execute() {
@@ -23,6 +53,7 @@ func Execute() {
 }
 
 func init() {
+	RootCmd.Flags().Bool("version", false, "print gok version")
 	RootCmd.AddCommand(runCmd)
 	RootCmd.AddCommand(logsCmd)
 }
