ad6ec912f7f5ea4bf0c8c0531c7f9c2f114bed66
oliverpool <git@olivier.pfad.fr>
2024-07-05 14:34:48 +0200

    use table-driven tests instead of f-tests

--- packer/gotool_test.go
+++ packer/gotool_test.go
@@ -3,29 +3,45 @@ package packer
 import "testing"
 
 func TestPkgBasename(t *testing.T) {
-	f := func(p Pkg, wantBasename string) {
-		t.Helper()
-		got := p.Basename()
-		if got != wantBasename {
-			t.Errorf("pkg.Basename got %q, want %q", got, wantBasename)
-		}
+	tests := []struct {
+		pkg          Pkg
+		wantBasename string
+	}{
+		{
+			Pkg{
+				Target: "target-name",
+			},
+			"target-name",
+		},
+		{
+			Pkg{
+				ImportPath: "example.com/import/path",
+				Target:     "target-name",
+			},
+			"target-name",
+		},
+		{
+			Pkg{
+				ImportPath: "example.com/import/path",
+				Target:     "",
+			},
+			"path",
+		},
+		{
+			Pkg{
+				ImportPath: "example.com/import/path/v2",
+				Target:     "",
+			},
+			"path",
+		},
 	}
 
-	f(Pkg{
-		Target: "target-name",
-	}, "target-name")
-
-	f(Pkg{
-		ImportPath: "example.com/import/path",
-		Target:     "target-name",
-	}, "target-name")
-
-	f(Pkg{
-		ImportPath: "example.com/import/path",
-		Target:     "",
-	}, "path")
-	f(Pkg{
-		ImportPath: "example.com/import/path/v2",
-		Target:     "",
-	}, "path")
+	for _, tt := range tests {
+		t.Run(tt.pkg.Target+"/"+tt.pkg.ImportPath, func(t *testing.T) {
+			got := tt.pkg.Basename()
+			if got != tt.wantBasename {
+				t.Errorf("pkg.Basename got %q, want %q", got, tt.wantBasename)
+			}
+		})
+	}
 }
