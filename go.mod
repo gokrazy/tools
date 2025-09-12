19d09ffb32b78f5e868812eeef905e7452d391f4
Michael Stapelberg <michael@stapelberg.de>
2025-09-12 09:34:42 +0200

    switch from breml/rootcerts to x/crypto fallback bundle
    related to https://github.com/golang/go/issues/69898

--- go.mod
+++ go.mod
@@ -1,9 +1,10 @@
 module github.com/gokrazy/tools
 
-go 1.24
+go 1.24.0
+
+toolchain go1.24.6
 
 require (
-	github.com/breml/rootcerts v0.2.21
 	github.com/donovanhide/eventsource v0.0.0-20210830082556-c59027999da0
 	github.com/gokrazy/gokapi v0.0.0-20250222080418-e140e9c461d8
 	github.com/gokrazy/internal v0.0.0-20250526201501-559979153369
@@ -12,6 +13,7 @@ require (
 	github.com/mattn/go-isatty v0.0.20
 	github.com/spf13/cobra v1.9.1
 	github.com/spf13/pflag v1.0.6
+	golang.org/x/crypto/x509roots/fallback v0.0.0-20250911151450-96dc232fbd79
 	golang.org/x/mod v0.24.0
 	golang.org/x/sync v0.14.0
 	golang.org/x/sys v0.33.0
--- internal/packer/cacerts.go
+++ internal/packer/cacerts.go
@@ -1,10 +1,11 @@
 package packer
 
 import (
+	"encoding/pem"
 	"os"
 	"path/filepath"
 
-	"github.com/breml/rootcerts/embedded"
+	"golang.org/x/crypto/x509roots/fallback/bundle"
 )
 
 func (pack *Pack) findSystemCertsPEM() (string, error) {
@@ -14,6 +15,7 @@ func (pack *Pack) findSystemCertsPEM() (string, error) {
 	defer func() {
 		log.Printf("Loading system CA certificates from %s", source)
 	}()
+
 	// On Linux, we can copy the operating system’s certificate store.
 	// certFiles is defined in cacerts_linux.go (or defined as empty in
 	// cacertsstub.go on non-Linux):
@@ -37,7 +39,18 @@ func (pack *Pack) findSystemCertsPEM() (string, error) {
 		return string(b), nil
 	}
 
-	// Fall back to github.com/breml/rootcerts, i.e. the bundled Mozilla CA list:
-	source = "bundled Mozilla CA list"
-	return embedded.MozillaCACertificatesPEM(), nil
+	// Fall back to the x/crypto fallback bundle root certificates:
+	source = "bundled x509roots/fallback/bundle"
+	return xrf(), nil
+}
+
+func xrf() string {
+	var certs []byte
+	for c := range bundle.Roots() {
+		certs = append(certs, pem.EncodeToMemory(&pem.Block{
+			Type:  "CERTIFICATE",
+			Bytes: c.Certificate,
+		})...)
+	}
+	return string(certs)
 }
